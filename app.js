79cf6998bd1adfabc7a7af8708415a074dd5d2cf
Jamie Cook <jamie.ereth@gmail.com>
2020-11-02 20:49:33 -0600

    local storage almost works

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
-const li = document.querySelector('li');
+const close = document.querySelectorAll('.close');
 const ul = document.querySelector('#myUL');
 const addBtn = document.querySelector('.addBtn');
 const input = document.querySelector('#myInput');
-JSON.parse(localStorage.getItem('list'));
+const todoArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const item = document.querySelectorAll('#text');
 const removeAll = document.querySelector('#removed');
+localStorage.setItem('items',JSON.stringify(todoArray));
+const data = JSON.parse(localStorage.getItem('items'));
 removeAll.addEventListener('click', function() {
 	// ul.parentNode.removeChild(ul);
 	document.querySelectorAll('li').forEach((e) => e.remove());
@@ -19,14 +21,8 @@ for (i = 0; i < myLiItems.length; i++) {
 	myLiItems[i].appendChild(span);
 	
 }
-const close = document.querySelectorAll('.close');
-for (i = 0; i < close.length; i++) {
-	close[i].onclick = function() {
-		const div = this.parentElement;
-		div.style.display = 'none';
-		localStorage.removeItem('ul');
-	};
-}
+
+
 const list = document.querySelector('ul');
 list.addEventListener(
 	'click',
@@ -37,25 +33,40 @@ list.addEventListener(
 	},
 	false
 );
-addBtn.addEventListener('click', function(e) {
-	e.preventDefault();
+
+const liMaker = (text)=>{
 	const newItem = document.createElement('li');
 	newItem.innerText = input.value;
 	input.value = '';
 	ul.appendChild(newItem);
 	const myLiItems = document.querySelectorAll('li');
-	const span = document.createElement('SPAN');
+	const newSpan = document.createElement('SPAN');
 	const txt = document.createTextNode('\u00D7');
-	span.className = 'close';
-	span.appendChild(txt);
-	myLiItems[i].appendChild(span);
+	newSpan.className = 'close';
+	newSpan.appendChild(txt);
+	myLiItems[i].appendChild(newSpan);
+	todoArray.push(newItem.innerText);
+	localStorage.setItem('items',JSON.stringify(todoArray));
 	const close = document.querySelectorAll('.close');
 	for (i = 0; i < close.length; i++) {
 		close[i].onclick = function() {
 			const div = this.parentElement;
 			div.style.display = 'none';
+			localStorage.remove();
 		};
+		}
 	}
+addBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	liMaker(input.value);
+	
 });
-localStorage.setItem('list', JSON.stringify(ul))
-const data = JSON.parse(localStorage.getItem('list'))
+data.forEach((item)=>{
+	liMaker(item);
+});
+
+
+
+
+
+
